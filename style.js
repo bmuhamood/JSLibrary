25bd249555302615cf558b100f293d92312093f1
bmuhamood <bbosa2009@gmail.com>
2021-10-11 20:47:35 +0400

    completed books list section

--- style.js
+++ style.js
@@ -37,6 +37,7 @@ else
   book_records.push({
   "title":title,
   "author":author,
+  
 })
 localStorage.setItem("books",JSON.stringify(book_records));
 }
@@ -54,9 +55,8 @@ book_records=JSON.parse(localStorage.getItem("books"))?JSON.parse(localStorage.g
     {
       let addDiv=document.createElement('div');
   addDiv.className="row";
-  addDiv.innerHTML='<div class="col-sm-4" style="padding: 10px;">'+book_records[i].title+'</div><div class="col-sm-4" style="padding: 10px;">'+book_records[i].author+'</div>';
+  addDiv.innerHTML='<div class="col-sm-4" style="padding: 10px;">'+book_records[i].title+'</div><div class="col-sm-4" style="padding: 10px;">'+book_records[i].author+'</div><div class="col-sm-4" style="padding: 10px;">'+book_records[i].delete+'</div>';
   document.getElementById("showBooks").appendChild(addDiv);
-
     }
   }
   }
